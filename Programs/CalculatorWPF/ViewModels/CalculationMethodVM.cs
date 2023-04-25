df46a29c9ba12471fb0994e27183c2058ab7fbca
IronLittleFox <lukasz.felisek@gmail.com>
2023-04-25 10:24:29 +0200

    dodanie projektów

---
+++ Programs/CalculatorWPF/ViewModels/CalculationMethodVM.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using UtilsWPF;
+
+namespace CalculatorWPF.ViewModels
+{
+    class CalculationMethodVM : ObserverVM
+    {
+        public CalculationMethodVM()
+        {
+            ChoseVM = new CalculatorOnpVM();
+            ListOfViewModel = new List<ICalculatorVM>();
+            ListOfViewModel.Add(ChoseVM);
+            ListOfViewModel.Add(new CalculatorVM());
+            ListOfViewModel.Add(new CodeLockVM());
+        }
+
+        private ICalculatorVM _choseVM;
+        public ICalculatorVM ChoseVM
+        {
+            get { return _choseVM; }
+            set 
+            { 
+                _choseVM = value;
+                _choseVM.ClearCommand.Execute(null);
+                OnPropertyChanged(nameof(ChoseVM));
+                OnPropertyChanged(nameof(KeyDownCommand));
+            }
+        }
+
+        private List<ICalculatorVM> _listOfViewModel;
+        public List<ICalculatorVM> ListOfViewModel
+        {
+            get { return _listOfViewModel; }
+            set 
+            { 
+                _listOfViewModel = value;
+                OnPropertyChanged(nameof(ListOfViewModel));
+            }
+        }
+
+        public ICommand KeyDownCommand
+        {
+            get
+            {
+                return ChoseVM.KeyDownCommand;
+            }
+        }
+    }
+}
---
+++ Programs/ClockWpfApp/ViewModels/StopWatchViewModel.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using UtilitiesWpf;
+
+namespace ClockWpfApp.ViewModels
+{
+    class StopWatchViewModel : ObserverVM
+    {
+        private bool isStopWatchRunning = false;
+
+        private string timeStorage;
+        public string TimeStorage
+        {
+            get { return timeStorage; }
+            set 
+            { 
+                timeStorage = value;
+                OnPropertyChanged(nameof(TimeStorage));
+            }
+        }
+
+        private ICommand startCommand;
+        public ICommand StartCommand
+        {
+            get
+            {
+                if (startCommand == null)
+                    startCommand = new RelayCommand<object>(
+                        o =>
+                        {
+                            DateTime startTime = DateTime.Now;
+                            isStopWatchRunning = true;
+                            Task.Run(() =>
+                            {
+                                
+                                //int stopWatchMilisecond = 0;
+                                while (isStopWatchRunning)
+                                {
+                                    //stopWatchMilisecond++;
+                                    //TimeSpan timeSpan = TimeSpan.FromMilliseconds(stopWatchMilisecond);
+
+                                    TimeSpan timeSpan = DateTime.Now - startTime;
+                                    string stringTime = string.Format("{0:D2}:{1:D2}:{2:D2}:{3:D3}",
+                                        timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds, timeSpan.Milliseconds);
+                                    TimeStorage = stringTime;
+
+                                    Thread.Sleep(1000);
+                                    
+                                }
+                            });
+                        },
+                        o => !isStopWatchRunning
+                        );
+                return startCommand;
+            }
+        }
+
+        private ICommand stopCommand;
+        public ICommand StopCommand
+        {
+            get
+            {
+                if (stopCommand == null)
+                    stopCommand = new RelayCommand<object>(
+                        o =>
+                        {
+                            isStopWatchRunning = false;
+                        },
+                        o => isStopWatchRunning
+                        );
+                return stopCommand;
+            }
+        }
+
+        private int number;
+
+        public int Number
+        {
+            get { return number; }
+            set { number = value; OnPropertyChanged(nameof(Number)); }
+        }
+
+        private ICommand numberCommand;
+        public ICommand NumberCommand
+        {
+            get
+            {
+                if (numberCommand == null)
+                    numberCommand = new RelayCommand<object>(
+                        o =>
+                        {
+                            Number = 5;
+
+                            Task.Run(() =>
+                            {
+                                Number = Number + 2;
+                            });
+                            Task.Run(() =>
+                            {
+                                Number = Number * 2;
+                            });
+                        }
+                        );
+                return numberCommand;
+            }
+        }
+    }
+}
