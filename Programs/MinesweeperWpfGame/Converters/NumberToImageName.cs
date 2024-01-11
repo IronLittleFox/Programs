5e4a09f08a1d99bb335911f0c7baa6344b81d240
Lukasz <lukasz.felisek@ezse.pl>
2024-01-11 10:42:54 +0100

    poprawka gry

---
+++ Programs/MinesweeperWpfGame/Converters/NumberToImageName.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace MinesweeperWpfGame.Converters
+{
+    public class NumberToImageName : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string? strNumber = value?.ToString()?.ToLower();
+            if (strNumber == "")
+                strNumber = "empty";
+            if (strNumber == "m")
+                strNumber = "mine";
+            if (strNumber == "?")
+                strNumber = "questionMark";
+
+            BitmapImage bitmapImage = new BitmapImage();
+            bitmapImage.BeginInit();
+            bitmapImage.UriSource = new Uri("Images\\" + strNumber + ".png", UriKind.RelativeOrAbsolute);
+            bitmapImage.EndInit();
+
+            return bitmapImage;
+
+            //return color + type + ".png";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
